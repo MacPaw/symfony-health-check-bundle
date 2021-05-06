b911ba62bc6902d59c3604a86d9180587069ea9d
Yozhef <hisemjo@gmail.com>
2021-05-06 14:54:07 +0300

    feat: add more test HealthController

--- tests/Integration/Controller/HealthControllerTest.php
+++ tests/Integration/Controller/HealthControllerTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace SymfonyHealthCheckBundle\Tests\Integration\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use SymfonyHealthCheckBundle\Check\StatusUpCheck;
+use SymfonyHealthCheckBundle\Controller\HealthController;
+use TypeError;
 
 class HealthControllerTest extends WebTestCase
 {
@@ -16,5 +19,24 @@ class HealthControllerTest extends WebTestCase
         $response = $client->getResponse();
         self::assertSame(200, $response->getStatusCode());
         self::assertSame(json_encode([]), $response->getContent());
+    }
+
+    public function testAddCheck(): void
+    {
+        $healthController = new HealthController();
+        $healthController->addHealthCheck(new StatusUpCheck());
+
+        $response = $healthController->healthCheckAction();
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertSame(json_encode([['status' => 'up']]), $response->getContent());
+    }
+
+    public function testAddCheckFailed(): void
+    {
+        self::expectException(TypeError::class);
+
+        $healthController = new HealthController();
+        $healthController->addHealthCheck(new HealthController());
     }
 }
