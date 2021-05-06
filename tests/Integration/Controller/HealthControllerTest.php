b826a58ac2710554c251b8926c8f32f076945657
Yozhef <hisemjo@gmail.com>
2021-05-06 18:59:54 +0300

    feat: add more test

--- tests/Integration/Controller/HealthControllerTest.php
+++ tests/Integration/Controller/HealthControllerTest.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace SymfonyHealthCheckBundle\Tests\Integration\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use SymfonyHealthCheckBundle\Check\DoctrineCheck;
+use SymfonyHealthCheckBundle\Check\EnvironmentCheck;
 use SymfonyHealthCheckBundle\Check\StatusUpCheck;
 use SymfonyHealthCheckBundle\Controller\HealthController;
 use TypeError;
@@ -21,7 +25,7 @@ class HealthControllerTest extends WebTestCase
         self::assertSame(json_encode([]), $response->getContent());
     }
 
-    public function testAddCheck(): void
+    public function testAddCheckStatusUp(): void
     {
         $healthController = new HealthController();
         $healthController->addHealthCheck(new StatusUpCheck());
@@ -30,6 +34,45 @@ class HealthControllerTest extends WebTestCase
 
         self::assertSame(200, $response->getStatusCode());
         self::assertSame(json_encode([['status' => 'up']]), $response->getContent());
+    }
+
+    public function testEnvironmentCheckCouldNotDetermine(): void
+    {
+        $healthController = new HealthController();
+        $healthController->addHealthCheck(new EnvironmentCheck(new ContainerBuilder()));
+
+        $response = $healthController->healthCheckAction();
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertSame(
+            json_encode([['name' => 'environment', 'environment' => 'Could not determine']]),
+            $response->getContent()
+        );
+    }
+
+    public function testDoctrineCheckServiceNotFoundException(): void
+    {
+        self::expectException(ServiceNotFoundException::class);
+
+        $healthController = new HealthController();
+        $healthController->addHealthCheck(new DoctrineCheck(new ContainerBuilder()));
+
+        $healthController->healthCheckAction();
+    }
+
+    public function testTwoCheck(): void
+    {
+        $healthController = new HealthController();
+        $healthController->addHealthCheck(new StatusUpCheck());
+        $healthController->addHealthCheck(new EnvironmentCheck(new ContainerBuilder()));
+
+        $response = $healthController->healthCheckAction();
+
+        self::assertSame(200, $response->getStatusCode());
+        self::assertSame(
+            json_encode([['status' => 'up'], ['name' => 'environment', 'environment' => 'Could not determine']]),
+            $response->getContent()
+        );
     }
 
     public function testAddCheckFailed(): void
