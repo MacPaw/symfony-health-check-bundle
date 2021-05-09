f4e916e92d8deda79c568b6507073092441f8505
Yozhef <hisemjo@gmail.com>
2021-05-09 15:17:25 +0300

    feat: add new test EnvironmentCheck

--- tests/Integration/Unit/Check/EnvironmentCheckTest.php
+++ tests/Integration/Unit/Check/EnvironmentCheckTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SymfonyHealthCheckBundle\Tests\Integration\Unit\Check;
 
+use Exception;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -35,6 +36,23 @@ class EnvironmentCheckTest extends TestCase
         self::assertArrayHasKey('name', $result);
         self::assertArrayHasKey('environment', $result);
         self::assertSame('test', $result['environment']);
+        self::assertSame('environment', $result['name']);
+    }
+
+    public function testStatusUpCheckFail(): void
+    {
+        $this->container
+            ->method('getParameter')
+            ->with('kernel.environment')
+            ->will(self::throwException(new Exception()));
+
+        $result = (new EnvironmentCheck($this->container))->check();
+
+        self::assertIsArray($result);
+        self::assertNotEmpty($result);
+        self::assertArrayHasKey('name', $result);
+        self::assertArrayHasKey('environment', $result);
+        self::assertSame('Could not determine', $result['environment']);
         self::assertSame('environment', $result['name']);
     }
 }
